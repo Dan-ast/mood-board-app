13c9f6ded2a20c306968c0daf85cccfd6f85a965
Anastasiia Dekret <anastasiiadekret@yahoo.com>
2025-10-23 15:23:50 +0200

    set up, styled and rendered the cards

--- src/index.js
+++ src/index.js
@@ -15,3 +15,31 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+export function MoodBoardItem({color, image, description}) {
+  return (
+    <div className="mood-board-item" style={{
+      backgroundColor: color,
+    }}>
+      <img className="mood-board-image" src={image} />
+      <h3 className="mood-board-text">{description}</h3>
+    </div>
+  )
+}
+
+export function MoodBoard() {
+  return (
+    <div>
+      <h1 className="mood-board-heading">Destination Mood Board</h1>
+      <div className="mood-board">
+        <MoodBoardItem color="#272343" image="https://cdn.freecodecamp.org/curriculum/labs/pathway.jpg" description="Caribbean" />
+        <MoodBoardItem color="#272343" image="https://cdn.freecodecamp.org/curriculum/labs/shore.jpg" description="Gawadar Beach" />
+        <MoodBoardItem color="#272343" image="https://cdn.freecodecamp.org/curriculum/labs/grass.jpg" description="Cape Town" />
+        <MoodBoardItem color="#272343" image="https://cdn.freecodecamp.org/curriculum/labs/ship.jpg" description="Suez Canal" />
+        <MoodBoardItem color="#272343" image="https://cdn.freecodecamp.org/curriculum/labs/santorini.jpg" description="Santorini" />
+        <MoodBoardItem color="#272343" image="https://cdn.freecodecamp.org/curriculum/labs/pigeon.jpg" description="Istanbul" />
+      </div>
+    </div>
+  );
+}
